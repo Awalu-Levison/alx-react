cd6fe2c3708764ed7cba27315a882fc7880893ef
Awalu-Levison <levisonawalu251@gmail.com>
2024-11-26 16:49:29 +0200

    JS file

--- 0x01-react_intro/task_0/dashboards/src/App.js
+++ 0x01-react_intro/task_0/dashboards/src/App.js
@@ -1,1 +1,13 @@
-// Code goes here
+import React from 'react'
+import ReactDOM from 'react-dom' 
+import './App.css'
+
+class App extends ReactDOM {
+	render() {
+		return (
+			<h1 class="App-header">School dashboard</h1>
+		)
+	}
+}
+
+export default App;
