10ce00fa713b88b0e7722abfee134c7251666606
Awalu-Levison <levisonawalu251@gmail.com>
2024-11-29 20:09:12 +0200

    Update the index file

--- 0x01-react_intro/task_1/dashboard/src/index.js
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -1,1 +1,21 @@
-// index js
+import React from 'react';
+import './index.css';
+import App from './App';
+import Notifications from './Notifications';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+
+    <React.StrictMode>
+        <App />
+        <div className="root-notifications">
+            <Notifications />
+        </div>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+// reportWebVitals(console.log)) is an example of a function
+// that can be used to measure performance of a react App
+// Find more here: https://bit.ly/CRA-vitals
+reportWebVitals();
