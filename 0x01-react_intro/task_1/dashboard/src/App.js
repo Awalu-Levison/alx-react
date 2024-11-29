157e5cb5eb7a9d4138a817367d5265569be5d697
Awalu-Levison <levisonawalu251@gmail.com>
2024-11-29 07:18:04 +0200

    Updated the footer section

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -1,1 +1,24 @@
-// rendering code goes here
+import './App.css';
+import { getFooterCopy, getFooterCopy, getFullYear } from './utils.js';
+import holberton_logo from './holberton-logo.jpg';
+
+function App() {
+  return (
+    <>
+      <div className="App-header">
+         <img src={holberton-logo} alt="logo" />
+      <h1>School dashboard</h1>
+      </div>
+
+      <div className="App-body">
+	<p>Login to access the full dashboard</p>
+      </div>
+
+      <div className="App-footer">
+	<p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
